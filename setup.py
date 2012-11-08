8b159cc67d0ceea59746899f6b25c2e43e64a6a2
Ask Solem <ask@celeryproject.org>
2012-11-08 19:23:49 +0000

    No longer supports Python 2.5

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ classes = """
     Development Status :: 5 - Production/Stable
     Programming Language :: Python
     Programming Language :: Python :: 2
-    Programming Language :: Python :: 2.5
     Programming Language :: Python :: 2.6
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3
