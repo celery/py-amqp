ce2d4dd1359bd31c0f2a19ed68361f69a970b4f5
Ask Solem <ask@celeryproject.org>
2013-08-16 14:44:10 +0100

    flakes

--- amqp/connection.py
+++ amqp/connection.py
@@ -29,7 +29,7 @@ except ImportError:
 from . import __version__
 from .abstract_channel import AbstractChannel
 from .channel import Channel
-from .exceptions import ChannelError, ConnectionError
+from .exceptions import Blocked, ChannelError, ConnectionError
 from .method_framing import MethodReader, MethodWriter
 from .serialization import AMQPWriter
 from .transport import create_transport
