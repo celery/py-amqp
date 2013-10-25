f78be6a474139a1be4aabccd9a087110b37b1fc8
Stephen Young <me@hownowstephen.com>
2013-10-25 02:42:20 -0400

    ensure messages carry the channel

--- amqp/abstract_channel.py
+++ amqp/abstract_channel.py
@@ -92,6 +92,7 @@ class AbstractChannel(object):
         if content is None:
             return amqp_method(self, args)
         else:
+            content.channel = self
             return amqp_method(self, args, content)
 
     #: Placeholder, the concrete implementations will have to
