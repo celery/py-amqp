aeb4f015e656d01f10e5f41ec6d4a0dcc929527a
Ask Solem <ask@celeryproject.org>
2014-05-20 22:41:58 +0100

    Mergeds celery.five, kombu.five and amqp.five

--- amqp/five.py
+++ amqp/five.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    celery.five
+    amqp.five
     ~~~~~~~~~~~
 
     Compatibility implementations of features
@@ -9,6 +9,32 @@
 
 """
 from __future__ import absolute_import
+
+__all__ = ['Counter', 'reload', 'UserList', 'UserDict', 'Queue', 'Empty',
+           'zip_longest', 'map', 'string', 'string_t',
+           'long_t', 'text_t', 'range', 'int_types', 'items', 'keys', 'values',
+           'nextfun', 'reraise', 'WhateverIO', 'with_metaclass',
+           'THREAD_TIMEOUT_MAX', 'format_d', 'monotonic']
+
+import io
+
+try:
+    from collections import Counter
+except ImportError:  # pragma: no cover
+    from collections import defaultdict
+
+    def Counter():  # noqa
+        return defaultdict(int)
+
+try:
+    buffer_t = buffer
+except NameError:  # pragma: no cover
+    # Py3 does not have buffer, only use this for isa checks.
+
+    class buffer_t(object):  # noqa
+        pass
+
+bytes_t = bytes
 
 ############## py3k #########################################################
 import sys
@@ -20,117 +46,14 @@ except NameError:                           # pragma: no cover
     from imp import reload                  # noqa
 
 try:
-    from UserList import UserList           # noqa
-except ImportError:                         # pragma: no cover
     from collections import UserList        # noqa
+except ImportError:                         # pragma: no cover
+    from UserList import UserList           # noqa
 
 try:
-    from UserDict import UserDict           # noqa
-except ImportError:                         # pragma: no cover
     from collections import UserDict        # noqa
-
-
-if PY3:
-    import builtins
-
-    from queue import Queue, Empty
-    from itertools import zip_longest
-    from io import StringIO, BytesIO
-
-    map = map
-    string = str
-    string_t = str
-    long_t = int
-    text_t = str
-    range = range
-    int_types = (int, )
-
-    open_fqdn = 'builtins.open'
-
-    def items(d):
-        return d.items()
-
-    def keys(d):
-        return d.keys()
-
-    def values(d):
-        return d.values()
-
-    def nextfun(it):
-        return it.__next__
-
-    exec_ = getattr(builtins, 'exec')
-
-    def reraise(tp, value, tb=None):
-        if value.__traceback__ is not tb:
-            raise value.with_traceback(tb)
-        raise value
-
-    class WhateverIO(StringIO):
-
-        def write(self, data):
-            if isinstance(data, bytes):
-                data = data.encode()
-            StringIO.write(self, data)
-
-else:
-    import __builtin__ as builtins  # noqa
-    from Queue import Queue, Empty  # noqa
-    from itertools import imap as map, izip_longest as zip_longest  # noqa
-    from StringIO import StringIO   # noqa
-    string = unicode                # noqa
-    string_t = basestring           # noqa
-    text_t = unicode
-    long_t = long                   # noqa
-    range = xrange
-    int_types = (int, long)
-
-    open_fqdn = '__builtin__.open'
-
-    def items(d):                   # noqa
-        return d.iteritems()
-
-    def keys(d):                    # noqa
-        return d.iterkeys()
-
-    def values(d):                  # noqa
-        return d.itervalues()
-
-    def nextfun(it):                # noqa
-        return it.next
-
-    def exec_(code, globs=None, locs=None):
-        """Execute code in a namespace."""
-        if globs is None:
-            frame = sys._getframe(1)
-            globs = frame.f_globals
-            if locs is None:
-                locs = frame.f_locals
-            del frame
-        elif locs is None:
-            locs = globs
-        exec("""exec code in globs, locs""")
-
-    exec_("""def reraise(tp, value, tb=None): raise tp, value, tb""")
-
-    BytesIO = WhateverIO = StringIO         # noqa
-
-
-def with_metaclass(Type, skip_attrs=set(['__dict__', '__weakref__'])):
-    """Class decorator to set metaclass.
-
-    Works with both Python 3 and Python 3 and it does not add
-    an extra class in the lookup order like ``six.with_metaclass`` does
-    (that is -- it copies the original class instead of using inheritance).
-
-    """
-
-    def _clone_with_metaclass(Class):
-        attrs = dict((key, value) for key, value in items(vars(Class))
-                     if key not in skip_attrs)
-        return Type(Class.__name__, Class.__bases__, attrs)
-
-    return _clone_with_metaclass
+except ImportError:                         # pragma: no cover
+    from UserDict import UserDict           # noqa
 
 ############## time.monotonic ################################################
 
@@ -186,3 +109,136 @@ try:
     from time import monotonic
 except ImportError:
     monotonic = _monotonic  # noqa
+
+############## Py3 <-> Py2 ###################################################
+
+if PY3:  # pragma: no cover
+    import builtins
+
+    from queue import Queue, Empty, Full, LifoQueue
+    from itertools import zip_longest
+
+    map = map
+    zip = zip
+    string = str
+    string_t = str
+    long_t = int
+    text_t = str
+    range = range
+    int_types = (int, )
+    module_name_t = str
+
+    open_fqdn = 'builtins.open'
+
+    def items(d):
+        return d.items()
+
+    def keys(d):
+        return d.keys()
+
+    def values(d):
+        return d.values()
+
+    def nextfun(it):
+        return it.__next__
+
+    exec_ = getattr(builtins, 'exec')
+
+    def reraise(tp, value, tb=None):
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
+
+else:
+    import __builtin__ as builtins  # noqa
+    from Queue import Queue, Empty, Full, LifoQueue  # noqa
+    from itertools import (               # noqa
+        imap as map,
+        izip as zip,
+        izip_longest as zip_longest,
+    )
+
+    string = unicode                # noqa
+    string_t = basestring           # noqa
+    text_t = unicode
+    long_t = long                   # noqa
+    range = xrange
+    module_name_t = str
+    int_types = (int, long)
+
+    open_fqdn = '__builtin__.open'
+
+    def items(d):                   # noqa
+        return d.iteritems()
+
+    def keys(d):                    # noqa
+        return d.iterkeys()
+
+    def values(d):                  # noqa
+        return d.itervalues()
+
+    def nextfun(it):                # noqa
+        return it.next
+
+    def exec_(code, globs=None, locs=None):  # pragma: no cover
+        """Execute code in a namespace."""
+        if globs is None:
+            frame = sys._getframe(1)
+            globs = frame.f_globals
+            if locs is None:
+                locs = frame.f_locals
+            del frame
+        elif locs is None:
+            locs = globs
+        exec("""exec code in globs, locs""")
+
+    exec_("""def reraise(tp, value, tb=None): raise tp, value, tb""")
+
+
+def with_metaclass(Type, skip_attrs={'__dict__', '__weakref__'}):
+    """Class decorator to set metaclass.
+
+    Works with both Python 2 and Python 3 and it does not add
+    an extra class in the lookup order like ``six.with_metaclass`` does
+    (that is -- it copies the original class instead of using inheritance).
+
+    """
+
+    def _clone_with_metaclass(Class):
+        attrs = {key: value for key, value in items(vars(Class))
+                 if key not in skip_attrs}
+        return Type(Class.__name__, Class.__bases__, attrs)
+
+    return _clone_with_metaclass
+
+############## threading.TIMEOUT_MAX #######################################
+try:
+    from threading import TIMEOUT_MAX as THREAD_TIMEOUT_MAX
+except ImportError:
+    THREAD_TIMEOUT_MAX = 1e10  # noqa
+
+############## format(int, ',d') ##########################
+
+if sys.version_info >= (2, 7):  # pragma: no cover
+    def format_d(i):
+        return format(i, ',d')
+else:  # pragma: no cover
+    def format_d(i):  # noqa
+        s = '%d' % i
+        groups = []
+        while s and s[-1].isdigit():
+            groups.append(s[-3:])
+            s = s[:-3]
+        return s + ','.join(reversed(groups))
+
+_SIO_write = io.StringIO.write
+_SIO_init = io.StringIO.__init__
+
+
+class WhateverIO(io.StringIO):
+
+    def __init__(self, v=None, *a, **kw):
+        _SIO_init(self, v.decode() if isinstance(v, bytes) else v, *a, **kw)
+
+    def write(self, data):
+        _SIO_write(self, data.decode() if isinstance(data, bytes) else data)
