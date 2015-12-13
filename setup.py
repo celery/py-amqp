0d8b567ce9e44a6ab8464971332b749ecff64d83
mr c0b <denc716@gmail.com>
2015-12-13 13:49:29 -0800

    exclude tests in wheel packaging :sunny:
    this will make the structure in wheel look like:
    
    ```console
    Archive:  ./dist/amqp-2.0.0rc1-py2.py3-none-any.whl
     Length   Method    Size  Cmpr    Date    Time   CRC-32   Name
    --------  ------  ------- ---- ---------- ----- --------  ----
        4814  Defl:N     1793  63% 2015-12-13 13:35 845f1f81  amqp/abstract_channel.py
       12801  Defl:N     3830  70% 2015-12-13 13:35 e7571a13  amqp/transport.py
        2100  Defl:N      757  64% 2015-12-13 13:35 eea088ac  amqp/utils.py
        6119  Defl:N     1895  69% 2015-12-13 13:35 ef0eff4d  amqp/method_framing.py
       71522  Defl:N    14536  80% 2015-12-13 13:35 517765af  amqp/channel.py
        6672  Defl:N     1934  71% 2015-12-13 13:35 0ce09720  amqp/exceptions.py
        2290  Defl:N     1094  52% 2015-12-13 13:35 790d2c96  amqp/__init__.py
        7099  Defl:N     2299  68% 2015-12-13 13:35 45896431  amqp/five.py
       22202  Defl:N     5805  74% 2015-12-13 13:35 d2243bdf  amqp/connection.py
        1888  Defl:N      606  68% 2015-12-13 13:35 e3169c45  amqp/spec.py
       10950  Defl:N     2874  74% 2015-12-13 13:35 d8594b24  amqp/promise.py
       17686  Defl:N     4054  77% 2015-12-13 13:35 005edc0e  amqp/serialization.py
        3748  Defl:N     1458  61% 2015-12-13 13:35 3053a91f  amqp/basic_message.py
         310  Defl:N      166  47% 2015-12-13 13:35 2f9d422a  amqp/protocol.py
        4030  Defl:N     1559  61% 2015-12-13 13:39 83ed2649  amqp-2.0.0rc1.dist-info/DESCRIPTION.rst
        1020  Defl:N      507  50% 2015-12-13 13:39 75f9fb8f  amqp-2.0.0rc1.dist-info/metadata.json
           5  Defl:N        7 -40% 2015-12-13 13:39 31e514b1  amqp-2.0.0rc1.dist-info/top_level.txt
         110  Defl:N       95  14% 2015-12-13 13:39 aa646eec  amqp-2.0.0rc1.dist-info/WHEEL
        4849  Defl:N     1837  62% 2015-12-13 13:39 1a639348  amqp-2.0.0rc1.dist-info/METADATA
        1550  Defl:N      970  37% 2015-12-13 13:39 fd7b1ae7  amqp-2.0.0rc1.dist-info/RECORD
    --------          -------  ---                            -------
      181765            48076  74%                            20 files
    ```

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setup(
     url=meta['homepage'],
     platforms=['any'],
     license='LGPL',
-    packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
+    packages=find_packages(exclude=['ez_setup', '*.tests']),
     zip_safe=False,
     install_requires=install_requires,
     tests_require=tests_require,
