922dd8249eee7bd8c37b4ad749201537ed2edfd7
Alan Justino <alan.justino@yahoo.com.br>
2016-06-02 12:04:16 -0300

    unicode_literals is breaking 2.7 tests

--- amqp/method_framing.py
+++ amqp/method_framing.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 
 from collections import defaultdict
 from struct import pack, unpack_from, pack_into
--- amqp/serialization.py
+++ amqp/serialization.py
@@ -19,7 +19,7 @@ Convert between bytestreams and higher-level AMQP types.
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 
 import calendar
 import sys
