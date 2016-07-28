9167f6569e5f48fcb37e6eda8caf9ce81de2dc93
Ask Solem <ask@celeryproject.org>
2016-07-28 13:04:38 -0700

    Argument to struct.unpack must be unicode on Python <=2.7.6
    Closes celery/kombu#609

--- amqp/transport.py
+++ amqp/transport.py
@@ -220,7 +220,7 @@ class _AbstractTransport(object):
         try:
             frame_header = read(7, True)
             read_frame_buffer += frame_header
-            frame_type, channel, size = unpack('>BHI', frame_header)
+            frame_type, channel, size = unpack(u'>BHI', frame_header)
             # >I is an unsigned int, but the argument to sock.recv is signed,
             # so we know the size can be at most 2 * SIGNED_INT_MAX
             if size > SIGNED_INT_MAX:
