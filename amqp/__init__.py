b4b57f9fff355546714e74f27e633eb3860a19b2
Ask Solem <ask@celeryproject.org>
2016-09-07 16:37:40 -0700

    Bump version: 2.0.3 → 2.1.0

--- amqp/__init__.py
+++ amqp/__init__.py
@@ -20,7 +20,7 @@ import re
 
 from collections import namedtuple
 
-__version__ = '2.0.3'
+__version__ = '2.1.0'
 __author__ = 'Barry Pederson'
 __maintainer__ = 'Ask Solem'
 __contact__ = 'pyamqp@celeryproject.org'
