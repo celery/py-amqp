8000e90688736971f2e2d9b14773477aa984b702
Ask Solem <ask@celeryproject.org>
2016-10-11 13:36:33 -0700

    Fix build

--- amqp/__init__.py
+++ amqp/__init__.py
@@ -20,10 +20,32 @@ import re
 
 from collections import namedtuple
 
-from .basic_message import Message
-from .channel import Channel
-from .connection import Connection
-from .exceptions import (
+__version__ = '2.1.0'
+__author__ = 'Barry Pederson'
+__maintainer__ = 'Ask Solem'
+__contact__ = 'pyamqp@celeryproject.org'
+__homepage__ = 'http://github.com/celery/py-amqp'
+__docformat__ = 'restructuredtext'
+
+# -eof meta-
+
+version_info_t = namedtuple('version_info_t', (
+    'major', 'minor', 'micro', 'releaselevel', 'serial',
+))
+
+# bumpversion can only search for {current_version}
+# so we have to parse the version here.
+_temp = re.match(
+    r'(\d+)\.(\d+).(\d+)(.+)?', __version__).groups()
+VERSION = version_info = version_info_t(
+    int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or '', '')
+del(_temp)
+del(re)
+
+from .basic_message import Message  # noqa
+from .channel import Channel        # noqa
+from .connection import Connection  # noqa
+from .exceptions import (           # noqa
     AMQPError,
     ConnectionError,
     RecoverableConnectionError,
@@ -51,29 +73,8 @@ from .exceptions import (
     InternalError,
     error_for_code,
 )
-from .utils import promise
+from .utils import promise      # noqa
 
-__version__ = '2.1.0'
-__author__ = 'Barry Pederson'
-__maintainer__ = 'Ask Solem'
-__contact__ = 'pyamqp@celeryproject.org'
-__homepage__ = 'http://github.com/celery/py-amqp'
-__docformat__ = 'restructuredtext'
-
-# -eof meta-
-
-version_info_t = namedtuple('version_info_t', (
-    'major', 'minor', 'micro', 'releaselevel', 'serial',
-))
-
-# bumpversion can only search for {current_version}
-# so we have to parse the version here.
-_temp = re.match(
-    r'(\d+)\.(\d+).(\d+)(.+)?', __version__).groups()
-VERSION = version_info = version_info_t(
-    int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or '', '')
-del(_temp)
-del(re)
 
 __all__ = [
     'Connection',
