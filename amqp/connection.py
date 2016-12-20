63d1a5891eb55b90900279852bd11f953fc43221
gord chung <gord@live.ca>
2016-12-20 23:49:58 +0000

    do not reconnect on collect
    collect is called to cleanup when there is a connection error but
    the backward compatibility code to connect when transport is called
    gets triggered. this doesn't seem right as this should be handled by
    ensure_connection.

--- amqp/connection.py
+++ amqp/connection.py
@@ -418,7 +418,7 @@ class Connection(AbstractChannel):
 
     def collect(self):
         try:
-            self.transport.close()
+            self._transport.close()
 
             temp_list = [x for x in values(self.channels) if x is not self]
             for ch in temp_list:
