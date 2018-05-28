c976dbf7b9e9d39ae85430eec4a9f442c5687bcb
Omer Katz <omer.drow@gmail.com>
2018-05-28 16:32:40 +0300

    Bump version: 2.3.0 → 2.3.1

--- amqp/__init__.py
+++ amqp/__init__.py
@@ -6,7 +6,7 @@ import re
 
 from collections import namedtuple
 
-__version__ = '2.3.0'
+__version__ = '2.3.1'
 __author__ = 'Barry Pederson'
 __maintainer__ = 'Ask Solem'
 __contact__ = 'pyamqp@celeryproject.org'
