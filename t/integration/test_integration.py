00b70589ed2a860a1d0614de2fec9f13ce18ccd0
Matus Valo <matusvalo@gmail.com>
2018-10-31 17:07:02 -0700

    Added integration tests for connection/channel open/close

--- t/integration/test_integration.py
+++ t/integration/test_integration.py
@@ -19,10 +19,8 @@ def ret_factory(method, channel=0, args=b'', arg_format=None):
 connection_testdata = (
     (spec.Connection.Blocked, '_on_blocked'),
     (spec.Connection.Unblocked, '_on_unblocked'),
-    (spec.Connection.OpenOk, '_on_open_ok'),
     (spec.Connection.Secure, '_on_secure'),
     (spec.Connection.CloseOk, '_on_close_ok'),
-    (spec.Connection.Tune, '_on_tune'),
 )
 
 
@@ -54,7 +52,7 @@ SERVER_CAPABILITIES = {
         'per_consumer_qos': True,
         'direct_reply_to': True
     },
-    'cluster_name': 'rabbit@t3dredis01.solar.cat.com',
+    'cluster_name': 'rabbit@broker.com',
     'copyright': 'Copyright (C) 2007-2018 Pivotal Software, Inc.',
     'information': 'Licensed under the MPL.  See http://www.rabbitmq.com/',
     'platform': 'Erlang/OTP 20.3.8.9',
@@ -63,33 +61,42 @@ SERVER_CAPABILITIES = {
 }
 
 
+def handshake(conn, transport_mock):
+    # Helper function simulating connection handshake with server
+    transport_mock().read_frame.side_effect = [
+        ret_factory(
+            spec.Connection.Start, channel=0,
+            args=(
+                0, 9, SERVER_CAPABILITIES, 'AMQPLAIN PLAIN', 'en_US'
+            ),
+            arg_format='ooFSS'
+        ),
+        ret_factory(
+            spec.Connection.Tune, channel=0,
+            args=(2047, 131072, 60), arg_format='BlB'
+        ),
+        ret_factory(
+            spec.Connection.OpenOk, channel=0
+        )
+    ]
+    conn.connect()
+    transport_mock().read_frame.reset_mock(return_value=True, side_effect=True)
+
+
 class test_integration:
 
     def test_connect(self):
         # Test checking connection handshake
         frame_writer_cls_mock = Mock()
+        on_open_mock = Mock()
         frame_writer_mock = frame_writer_cls_mock()
-        conn = Connection(frame_writer=frame_writer_cls_mock)
+        conn = Connection(
+            frame_writer=frame_writer_cls_mock, on_open=on_open_mock
+        )
 
         with patch.object(conn, 'Transport') as transport_mock:
-            # Mocked responses from Server
-            transport_mock().read_frame.side_effect = [
-                ret_factory(
-                    spec.Connection.Start, channel=0,
-                    args=(
-                        0, 9, SERVER_CAPABILITIES, 'AMQPLAIN PLAIN', 'en_US'
-                    ),
-                    arg_format='ooFSS'
-                ),
-                ret_factory(
-                    spec.Connection.Tune, channel=0,
-                    args=(2047, 131072, 60), arg_format='BlB'
-                ),
-                ret_factory(
-                    spec.Connection.OpenOk, channel=0
-                )
-            ]
-            conn.connect()
+            handshake(conn, transport_mock)
+            on_open_mock.assert_called_once_with(conn)
             # Expected responses from client
             frame_writer_mock.assert_has_calls(
                 [
@@ -119,33 +126,99 @@ class test_integration:
                 ]
             )
 
+    def test_connection_close(self):
+        # Test checking closing connection
+        frame_writer_cls_mock = Mock()
+        frame_writer_mock = frame_writer_cls_mock()
+        conn = Connection(frame_writer=frame_writer_cls_mock)
+        with patch.object(conn, 'Transport') as transport_mock:
+            handshake(conn, transport_mock)
+            frame_writer_mock.reset_mock()
+            # Inject CloseOk response from broker
+            transport_mock().read_frame.return_value = ret_factory(
+                spec.Connection.CloseOk,
+                args=(1, False),
+                arg_format='Lb'
+            )
+            t = conn.transport
+            conn.close()
+            frame_writer_mock.assert_called_once_with(
+                1, 0, spec.Connection.Close, dumps('BsBB', (0, '', 0, 0)), None
+            )
+            t.close.assert_called_once_with()
+
     @pytest.mark.parametrize("method, callback", connection_testdata)
     def test_connection_methods(self, method, callback):
         # Test verifying that proper Connection callback is called when
         # given method arrived from Broker.
         with patch.object(Connection, callback) as callback_mock:
             conn = Connection()
-            # Protect to get handshake from server during conn.connect()
-            conn._handshake_complete = True
             with patch.object(conn, 'Transport') as transport_mock:
-                conn.connect()
+                handshake(conn, transport_mock)
+                # Inject desired method
                 transport_mock().read_frame.return_value = ret_factory(
                     method, channel=0, args=(1, False), arg_format='Lb'
                 )
                 conn.drain_events(0)
                 callback_mock.assert_called_once()
 
+    def test_channel_open_close(self):
+        # Test checking opening and closing channel
+        frame_writer_cls_mock = Mock()
+        conn = Connection(frame_writer=frame_writer_cls_mock)
+        with patch.object(conn, 'Transport') as transport_mock:
+            handshake(conn, transport_mock)
+
+            channel_id = 1
+            transport_mock().read_frame.side_effect = [
+                # Inject Open Handshake
+                ret_factory(
+                    spec.Channel.OpenOk,
+                    channel=channel_id,
+                    args=(1, False),
+                    arg_format='Lb'
+                ),
+                # Inject close method
+                ret_factory(
+                    spec.Channel.CloseOk,
+                    channel=channel_id,
+                    args=(1, False),
+                    arg_format='Lb'
+                )
+            ]
+
+            frame_writer_mock = frame_writer_cls_mock()
+            frame_writer_mock.reset_mock()
+
+            on_open_mock = Mock()
+            ch = conn.channel(channel_id=channel_id, callback=on_open_mock)
+            on_open_mock.assert_called_once_with(ch)
+            assert ch.is_open is True
+
+            ch.close()
+            frame_writer_mock.assert_has_calls(
+                [
+                    call(
+                        1, 1, spec.Channel.Open, dumps('s', ('',)),
+                        None
+                    ),
+                    call(
+                        1, 1, spec.Channel.Close, dumps('BsBB', (0, '', 0, 0)),
+                        None
+                    )
+                ]
+            )
+            assert ch.is_open is False
+
     @pytest.mark.parametrize("method, callback", channel_testdata)
     def test_channel_methods(self, method, callback):
         # Test verifying that proper Channel callback is called when
         # given method arrived from Broker
-        with patch.object(Channel, callback) as callback_mock, \
-                patch.object(Channel, '_on_close_ok') as close_ok_mock:
+        with patch.object(Channel, callback) as callback_mock:
             conn = Connection()
-            # Protect to get handshake from server during conn.connect()
-            conn._handshake_complete = True
             with patch.object(conn, 'Transport') as transport_mock:
-                conn.connect()
+                handshake(conn, transport_mock)
+
                 channel_id = 1
                 # Inject Open Handshake
                 transport_mock().read_frame.return_value = ret_factory(
@@ -154,11 +227,9 @@ class test_integration:
                     args=(1, False),
                     arg_format='Lb'
                 )
-                with patch.object(Channel, '_on_open_ok') as open_ok_mock:
-                    ch = conn.channel(channel_id=channel_id)
-                    open_ok_mock.assert_called_once()
-                # We need to call real method to open channel.
-                ch._on_open_ok()
+
+                conn.channel(channel_id=channel_id)
+
                 # Inject desired method
                 transport_mock().read_frame.return_value = ret_factory(
                     method,
@@ -168,13 +239,3 @@ class test_integration:
                 )
                 conn.drain_events(0)
                 callback_mock.assert_called_once()
-                # Inject close method
-                transport_mock().read_frame.return_value = ret_factory(
-                    spec.Channel.CloseOk,
-                    channel=channel_id,
-                    args=(1, False),
-                    arg_format='Lb'
-                )
-
-                ch.close()
-                close_ok_mock.assert_called_once()
