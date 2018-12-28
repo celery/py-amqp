fbc14ce0102a971bf4426ee8d285ac3d18f384e7
Matus Valo <matusvalo@gmail.com>
2018-12-27 17:03:29 -0800

    Improved empty AMQPError string representation

--- t/unit/test_exceptions.py
+++ t/unit/test_exceptions.py
@@ -1,16 +1,37 @@
 from __future__ import absolute_import, unicode_literals
 
 from case import Mock
+import pytest
 
+import amqp.exceptions
 from amqp.exceptions import AMQPError, error_for_code
+
+AMQP_EXCEPTIONS = [
+    'ConnectionError', 'ChannelError',
+    'RecoverableConnectionError', 'IrrecoverableConnectionError',
+    'RecoverableChannelError', 'IrrecoverableChannelError',
+    'ConsumerCancelled', 'ContentTooLarge', 'NoConsumers',
+    'ConnectionForced', 'InvalidPath', 'AccessRefused', 'NotFound',
+    'ResourceLocked', 'PreconditionFailed', 'FrameError', 'FrameSyntaxError',
+    'InvalidCommand', 'ChannelNotOpen', 'UnexpectedFrame', 'ResourceError',
+    'NotAllowed', 'AMQPNotImplementedError', 'InternalError',
+]
 
 
 class test_AMQPError:
 
     def test_str(self):
-        assert str(AMQPError())
+        assert str(AMQPError()) == '<AMQPError: unknown error>'
         x = AMQPError(method_sig=(50, 60))
-        assert str(x)
+        assert str(x) == '(50, 60): (0) None'
+        x = AMQPError('Test Exception')
+        assert str(x) == 'Test Exception'
+
+    @pytest.mark.parametrize("amqp_exception", AMQP_EXCEPTIONS)
+    def test_str_subclass(self, amqp_exception):
+        exp = '<{}: unknown error>'.format(amqp_exception)
+        exception_class = getattr(amqp.exceptions, amqp_exception)
+        assert str(exception_class()) == exp
 
 
 class test_error_for_code:
