2e0eed4954d1a77f6e19309b490e84daef5376b1
Omer Katz <omer.drow@gmail.com>
2019-02-27 01:30:09 +0200

    Shorten benchmark.

--- t/benchmarks/test_benchmarks.py
+++ t/benchmarks/test_benchmarks.py
@@ -5,23 +5,23 @@ from amqp.serialization import loads, dumps
 
 
 def test_deserialize(benchmark):
-    format = b'bobBlLbsbSTx' * 8
+    format = b'bobBlLbsbSTx'
     x = dumps(format, [
         True, 32, False, 3415, 4513134, 13241923419,
         True, b'thequickbrownfox', False, 'jumpsoverthelazydog',
         datetime(2015, 3, 13, 10, 23),
         b'thequick\xff'
-    ] * 8)
+    ])
     benchmark(loads, format, x)
 
 
 def test_deserialize_pure_python(benchmark):
     os.environ['PYAMQP_DUMPS_SKIP_SPEEDUPS'] = "True"
-    format = b'bobBlLbsbSTx' * 8
+    format = b'bobBlLbsbSTx'
     x = dumps(format, [
         True, 32, False, 3415, 4513134, 13241923419,
         True, b'thequickbrownfox', False, 'jumpsoverthelazydog',
         datetime(2015, 3, 13, 10, 23),
         b'thequick\xff'
-    ] * 8)
+    ])
     benchmark(loads, format, x)
