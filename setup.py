2650536bd62c0ce27dec0df1172d6224de224778
Hervé Beraud <hberaud@redhat.com>
2019-11-14 18:40:13 +0100

    Fix pypi classifier development status (#294)
    The version 5.0 is currently an alpha version
    but pypi classifiers tag this version as a production/stable version,
    this is wrong, this version is a pre-alpha version.
    
    The main issue is that by using pip or requirements file to install
    py-amqp it will pull and install the pre-alpha version (5.0) instead of
    the stable version 2.5.2.
    
    Normally pip don't install non stable version except if user
    explicitly specify to install a non stable version.
    
    Example with oslo.messaging:
    
    ```
    $ pip install oslo.messaging
    ```
    
    The previous command will pull py-amqp 5.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ NAME = 'amqp'
 # -*- Classifiers -*-
 
 classes = """
-    Development Status :: 5 - Production/Stable
+    Development Status :: 2 - Pre-Alpha
     Programming Language :: Python
     Programming Language :: Python :: 2
     Programming Language :: Python :: 2.7
