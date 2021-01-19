41b13dd408a48cc5d789d773c3c62e6c638734ba
Asif Saif Uddin (Auvi) <auvipy@gmail.com>
2021-01-19 19:40:15 +0600

    import types to init

--- amqp/__init__.py
+++ amqp/__init__.py
@@ -39,6 +39,7 @@ from .exceptions import (AccessRefused, AMQPError,  # noqa
                          NotFound, PreconditionFailed, RecoverableChannelError,
                          RecoverableConnectionError, ResourceError,
                          ResourceLocked, UnexpectedFrame, error_for_code)
+from .types import ChannelT, ConnectionT, MessageT
 from .utils import promise  # noqa
 
 __all__ = (
