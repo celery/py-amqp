9517a4d3fac2fe5ed07b93e9682553084efc5a22
Asif Saif Uddin {"Auvi":"অভি"} <auvipy@gmail.com>
2026-09-07 20:44:39 +0600

    Update Python version requirements in setup.py (#467)

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ classes = """
     Programming Language :: Python
     Programming Language :: Python :: 3 :: Only
     Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.9
     Programming Language :: Python :: 3.10
     Programming Language :: Python :: 3.11
     Programming Language :: Python :: 3.12
     Programming Language :: Python :: 3.13
+    Programming Language :: Python :: 3.14
     Programming Language :: Python :: Implementation :: CPython
     Programming Language :: Python :: Implementation :: PyPy
     License :: OSI Approved :: BSD License
@@ -133,7 +133,7 @@ setuptools.setup(
     platforms=['any'],
     license='BSD',
     classifiers=classifiers,
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=reqs('default.txt'),
     setup_requires=setup_requires,
     tests_require=reqs('test.txt'),
